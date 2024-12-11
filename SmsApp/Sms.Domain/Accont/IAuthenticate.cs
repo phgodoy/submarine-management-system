2d24f0e728374205380b96fa2176f4a0358adf39
Pepe <phgodoy12@gmail.com>
2024-12-10 23:38:40 -0300

    Created Domain interfaces
    this review created authenticate and seed user roles interfaces for using login module.

---
+++ SmsApp/Sms.Domain/Accont/IAuthenticate.cs
@@ -0,0 +1,12 @@
+﻿namespace Sms.Domain.Accont
+{
+    public interface IAuthenticate
+    {
+        Task<bool> Authenticate(string email, string password);
+
+        Task<bool> RegisterUser(string email, string password);
+
+        Task Logout();
+
+    }
+}
