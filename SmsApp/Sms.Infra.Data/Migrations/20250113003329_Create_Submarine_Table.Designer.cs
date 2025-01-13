0421dc9660c3c40913a23b26b3d5f2d697d336a2
Pepe <phgodoy12@gmail.com>
2025-01-12 21:37:10 -0300

    Create Migrations Submarine
    This review creating new table in the database.

--- SmsApp/Sms.Infra.Data/Migrations/20250113003329_Create_Submarine_Table.Designer.cs
+++ SmsApp/Sms.Infra.Data/Migrations/20250113003329_Create_Submarine_Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sms.Infra.Data.Context;
 
@@ -11,9 +12,11 @@ using Sms.Infra.Data.Context;
 namespace Sms.Infra.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250113003329_Create_Submarine_Table")]
+    partial class Create_Submarine_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
