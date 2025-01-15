7dd7d9874bde0ed5e9d20fb2a8c7785e07943e71
Pepe <phgodoy12@gmail.com>
2025-01-14 22:52:00 -0300

    Create migration of MaintenanceLog
    this review add migrations of MaintenanceLog
    for link submarine tables and submarine system tables

---
+++ SmsApp/Sms.Infra.Data/Migrations/20250115014612_Create_MaintenanceLog_Table.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sms.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_MaintenanceLog_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MaintenanceLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubmarineSystemId = table.Column<int>(type: "int", nullable: false),
+                    MaintenanceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TechnicianName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaintenanceLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MaintenanceLogs_SubmarineSystems_SubmarineSystemId",
+                        column: x => x.SubmarineSystemId,
+                        principalTable: "SubmarineSystems",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaintenanceLogs_SubmarineSystemId",
+                table: "MaintenanceLogs",
+                column: "SubmarineSystemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MaintenanceLogs");
+        }
+    }
+}
