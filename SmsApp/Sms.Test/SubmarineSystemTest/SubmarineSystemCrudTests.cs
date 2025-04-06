4f7c27df334536087880e52a8e378ceeef0cc514
Pepe <phgodoy12@gmail.com>
2025-04-06 18:31:22 -0300

    test(domain): created CRUD unit tests for Submarine and SubmarineSystem domain entities

---
+++ SmsApp/Sms.Test/SubmarineSystemTest/SubmarineSystemCrudTests.cs
@@ -0,0 +1,93 @@
+﻿using Sms.Domain.Entities;
+using Sms.Domain.Enums;
+using Sms.Domain.Validation;
+using Xunit;
+
+namespace Sms.Test.SubmarineSystemTest
+{
+    public class SubmarineSystemCrudTests
+    {
+        [Fact]
+        public void Create_ValidSubmarineSystem_ShouldNotThrowException()
+        {
+            var exception = Record.Exception(() =>
+                new SubmarineSystem(1, "Sonar", "Navigation", SystemStatusEnum.InOperation, DateTime.Now.AddDays(-10)));
+
+            Assert.Null(exception);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void Create_InvalidName_ShouldThrowException(string invalidName)
+        {
+            var ex = Assert.Throws<DomainExceptionValidation>(() =>
+                new SubmarineSystem(1, invalidName, "Navigation", SystemStatusEnum.InOperation, DateTime.Now));
+
+            Assert.Equal("Invalid: name is required", ex.Message);
+        }
+
+        [Fact]
+        public void Create_ShortName_ShouldThrowException()
+        {
+            var ex = Assert.Throws<DomainExceptionValidation>(() =>
+                new SubmarineSystem(1, "AB", "Navigation", SystemStatusEnum.InOperation, DateTime.Now));
+
+            Assert.Equal("Invalid: name is too short", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void Create_InvalidType_ShouldThrowException(string invalidType)
+        {
+            var ex = Assert.Throws<DomainExceptionValidation>(() =>
+                new SubmarineSystem(1, "Radar", invalidType, SystemStatusEnum.InOperation, DateTime.Now));
+
+            Assert.Equal("Invalid: type is required", ex.Message);
+        }
+
+        [Fact]
+        public void Create_InvalidSystemStatus_ShouldThrowException()
+        {
+            var ex = Assert.Throws<DomainExceptionValidation>(() =>
+                new SubmarineSystem(1, "Radar", "Sensor", (SystemStatusEnum)999, DateTime.Now));
+
+            Assert.Equal("Invalid: operational status is not valid", ex.Message);
+        }
+
+        [Fact]
+        public void Create_WithNegativeId_ShouldThrowException()
+        {
+            var ex = Assert.Throws<DomainExceptionValidation>(() =>
+                new SubmarineSystem(-1, 1, "Radar", "Sensor", SystemStatusEnum.InOperation, DateTime.Now));
+
+            Assert.Equal("Invalid id value", ex.Message);
+        }
+
+        [Fact]
+        public void Update_ShouldModifyProperties()
+        {
+            var system = new SubmarineSystem(1, "Radar", "Sensor", SystemStatusEnum.InOperation, DateTime.Now.AddDays(-5));
+
+            system.Update(2, "Sonar", "Navigation", SystemStatusEnum.Committed, DateTime.Now);
+
+            Assert.Equal(2, system.SubmarineId);
+            Assert.Equal("Sonar", system.Name);
+            Assert.Equal("Navigation", system.Type);
+            Assert.Equal(SystemStatusEnum.Committed, system.SystemStatusId);
+        }
+
+        [Fact]
+        public void UpdateOperationalSystem_ShouldChangeStatus()
+        {
+            var system = new SubmarineSystem(1, "Radar", "Sensor", SystemStatusEnum.InOperation, DateTime.Now);
+
+            system.UpdateOperationalSystem(SystemStatusEnum.Disable);
+
+            Assert.Equal(SystemStatusEnum.Disable, system.SystemStatusId);
+        }
+    }
+}
---
+++ SmsApp/Sms.Test/SubmarineTest/SubmarineCrudTests.cs
@@ -0,0 +1,92 @@
+﻿using Sms.Domain.Entities;
+using Sms.Domain.Enums;
+using Sms.Domain.Validation;
+using System;
+using Xunit;
+
+namespace Sms.Test.SubmarineTest
+{
+    public class SubmarineCrudTests
+    {
+        [Fact]
+        public void Create_ValidSubmarine_ShouldNotThrowException()
+        {
+            var exception = Record.Exception(() =>
+                new Submarine("Nautilus", "Model-X", DateTime.Now.AddYears(-5), SubmarineStatusEnum.InOperation));
+
+            Assert.Null(exception);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void Create_InvalidName_ShouldThrowException(string invalidName)
+        {
+            var ex = Assert.Throws<DomainExceptionValidation>(() =>
+                new Sms.Domain.Entities.Submarine(invalidName, "Model-X", DateTime.Now, SubmarineStatusEnum.InOperation));
+
+            Assert.Equal("Invalid: Name is required", ex.Message);
+        }
+
+        [Fact]
+        public void Create_ShortName_ShouldThrowException()
+        {
+            var ex = Assert.Throws<DomainExceptionValidation>(() =>
+                new Sms.Domain.Entities.Submarine("AB", "Model-X", DateTime.Now, SubmarineStatusEnum.InOperation));
+
+            Assert.Equal("Invalid: Name is too short", ex.Message);
+        }
+
+        [Fact]
+        public void Create_InvalidModel_ShouldThrowException()
+        {
+            var ex = Assert.Throws<DomainExceptionValidation>(() =>
+                new Sms.Domain.Entities.Submarine("Nautilus", "", DateTime.Now, SubmarineStatusEnum.InOperation));
+
+            Assert.Equal("Invalid: Model is required", ex.Message);
+        }
+
+        [Fact]
+        public void Create_FutureCreationDate_ShouldThrowException()
+        {
+            var futureDate = DateTime.Now.AddDays(1);
+
+            var ex = Assert.Throws<DomainExceptionValidation>(() =>
+                new Sms.Domain.Entities.Submarine("Nautilus", "Model-X", futureDate, SubmarineStatusEnum.InOperation));
+
+            Assert.Equal("Invalid: creation date cannot be in the future", ex.Message);
+        }
+
+        [Fact]
+        public void Update_ShouldModifyProperties()
+        {
+            var submarine = new Sms.Domain.Entities.Submarine("OldName", "OldModel", DateTime.Now.AddYears(-3), SubmarineStatusEnum.Deactivated);
+
+            submarine.Update("NewName", "NewModel", DateTime.Now.AddYears(-1), SubmarineStatusEnum.UnderMaintenance);
+
+            Assert.Equal("NewName", submarine.Name);
+            Assert.Equal("NewModel", submarine.Model);
+            Assert.Equal(SubmarineStatusEnum.UnderMaintenance, submarine.SubmarineStatusId);
+        }
+
+        [Fact]
+        public void UpdateSubmarineStatus_ShouldChangeStatus()
+        {
+            var submarine = new Sms.Domain.Entities.Submarine("Test", "Model-A", DateTime.Now.AddYears(-2), SubmarineStatusEnum.Deactivated);
+
+            submarine.UpdateSubmarineStatus(SubmarineStatusEnum.InOperation);
+
+            Assert.Equal(SubmarineStatusEnum.InOperation, submarine.SubmarineStatusId);
+        }
+
+        [Fact]
+        public void Create_WithNegativeId_ShouldThrowException()
+        {
+            var ex = Assert.Throws<DomainExceptionValidation>(() =>
+                new Sms.Domain.Entities.Submarine(-1, "ValidName", "Model", DateTime.Now.AddYears(-2), SubmarineStatusEnum.InOperation));
+
+            Assert.Equal("Invalid id value", ex.Message);
+        }
+    }
+}
